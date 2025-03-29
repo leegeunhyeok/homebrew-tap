159a546bfd20fe765392fad8b1ca1143ca7771e5
leegeunhyeok <dev.ghlee@gmail.com>
2025-03-29 18:49:14 +0900

    update download url

--- Formula/sourcemap-explorer.rb
+++ Formula/sourcemap-explorer.rb
@@ -4,10 +4,10 @@ class SourcemapExplorer < Formula
   version "1.0.0-alpha.4"
 
   if Hardware::CPU.intel?
-    url "https://github.com/leegeunhyeok/sourcemap-explorer/releases/download/#{version}/sourcemap-explorer-x86_64-apple-darwin.tar.gz"
+    url "https://github.com/leegeunhyeok/sourcemap-explorer/releases/download/#{version}/x86_64-apple-darwin.tar.gz"
     sha256 "b2897777e6e3c4d025504d38b400bae85d24469a2f5a1ce6946a32bb816c407a"
   elsif Hardware::CPU.arm?
-    url "https://github.com/leegeunhyeok/sourcemap-explorer/releases/download/#{version}/sourcemap-explorer-aarch64-apple-darwin.tar.gz"
+    url "https://github.com/leegeunhyeok/sourcemap-explorer/releases/download/#{version}/aarch64-apple-darwin.tar.gz"
     sha256 "676ba9dc6ac498d119ba18af72ff227943a0792d94c1edec54fffdbd09583fd5"
   end
 

a7bb361c156181e25f7e93bbe45634b75536d4d0
leegeunhyeok <dev.ghlee@gmail.com>
2025-03-29 18:46:33 +0900

    sourcemap-explorer@1.0.0-alpha.4

--- Formula/sourcemap-explorer.rb
+++ Formula/sourcemap-explorer.rb
@@ -1,14 +1,14 @@
 class SourcemapExplorer < Formula
   desc "Rust based sourcemap explorer for command-line interface"
   homepage "https://github.com/leegeunhyeok/sourcemap-explorer"
-  version "1.0.0-alpha.3"
+  version "1.0.0-alpha.4"
 
   if Hardware::CPU.intel?
     url "https://github.com/leegeunhyeok/sourcemap-explorer/releases/download/#{version}/sourcemap-explorer-x86_64-apple-darwin.tar.gz"
-    sha256 "454fed4fd90c9feb8af95aa8a1159584f05e82a4c7bfb1711f9a8de119b8891e"
+    sha256 "b2897777e6e3c4d025504d38b400bae85d24469a2f5a1ce6946a32bb816c407a"
   elsif Hardware::CPU.arm?
     url "https://github.com/leegeunhyeok/sourcemap-explorer/releases/download/#{version}/sourcemap-explorer-aarch64-apple-darwin.tar.gz"
-    sha256 "7c15c6435ef52af2f5b01cba56d40e3a817fe489573494a0442c09d01f799acf"
+    sha256 "676ba9dc6ac498d119ba18af72ff227943a0792d94c1edec54fffdbd09583fd5"
   end
 
   def install
